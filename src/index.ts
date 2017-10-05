396cd9207b3ae201fe03780853cc3f8dd717a2e5
Homa Wong <homawong@gmail.com>
2017-10-05 10:34:09 -0700

    Use `object` instead of `Object` in type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import defaults = require('lodash.defaults')
 
-export function unpartial<T extends Object, U, R extends Object>(obj: U, src1: T, src2?: R): T & R {
+export function unpartial<T extends object, U, R extends object>(obj: U, src1: T, src2?: R): T & R {
   if (src1 === undefined || src1 === null)
     return obj as any
   return defaults(obj, src1, src2)
