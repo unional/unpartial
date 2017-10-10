642227c65cbedc8861a5af25486df680bddade3f
Homa Wong <homawong@gmail.com>
2017-10-10 01:02:33 -0700

    Support undefined partial

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import defaults = require('lodash.defaults')
 
-export function unpartial<T extends object>(base: T, partial: Partial<T>): T
-export function unpartial<T extends object, R extends object = {}>(superBase: R, base: T, partial: Partial<T>): T & R
+// `partial: Partial<T> | undefined` is better than `partial?: Partial<T>` in this case.
+// I want to make sure `partial` is passed in, while it can be optional, i.e. undefined, at caller context.
+export function unpartial<T extends object>(base: T, partial: Partial<T> | undefined): T
+export function unpartial<T extends object, R extends object = {}>(superBase: R, base: T, partial: Partial<T> | undefined): T & R
 export function unpartial(arg1, arg2, arg3?) {
   if (arguments.length === 3) {
     const superBase = arg1
