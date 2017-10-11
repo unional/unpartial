5600babc5f35cae56c4d7d28f428ad4334a0886b
Homa Wong <homawong@gmail.com>
2017-10-11 11:36:07 -0700

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,23 @@ export function unpartial<T extends object>(base: T, partial: Partial<T> | undef
 export function unpartial<T extends object, R extends object = {}>(superBase: R, base: T, partial: Partial<T> | undefined): T & R
 export function unpartial(arg1, arg2, arg3?) {
   if (arguments.length === 3) {
-    const superBase = arg1
-    const base = arg2
-    const partial = arg3
-    if (superBase === undefined || superBase === null)
-      return superBase
-    if (base === undefined || base === null)
-      return base
-    return defaults({}, partial, base, superBase)
+    return unpartialTrio(arg1, arg2, arg3)
   }
   else {
-    const base = arg1
-    const partial = arg2
-    if (base === undefined || base === null)
-      return base
-    return defaults({}, partial, base)
+    return unpartialDuo(arg1, arg2)
   }
+}
+
+function unpartialTrio(superBase, base, partial) {
+  if (superBase === undefined || superBase === null)
+    return superBase
+  if (base === undefined || base === null)
+    return base
+  return defaults({}, partial, base, superBase)
+}
+
+function unpartialDuo(base, partial) {
+  if (base === undefined || base === null)
+    return base
+  return defaults({}, partial, base)
 }
