5415c0451b4f5a64f5ce704fae6d38d0d5136773
Homa Wong <homawong@gmail.com>
2017-10-14 14:03:16 -0700

    Support optional 2nd Argument (#7)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import defaults = require('lodash.defaults')
 // `partial: Partial<T> | undefined` is better than `partial?: Partial<T>` in this case.
 // I want to make sure `partial` is passed in, while it can be optional, i.e. undefined, at caller context.
 export function unpartial<T extends object>(base: T, partial: Partial<T> | undefined): T
-export function unpartial<T extends object, R extends object = {}>(superBase: R, base: T, partial: Partial<T> | undefined): T & R
+export function unpartial<T extends object, R extends object = {}>(superBase: R, base: T | undefined, partial: Partial<T> | undefined): T & R
 export function unpartial(arg1, arg2, arg3?) {
   if (arguments.length === 3) {
     return unpartialTrio(arg1, arg2, arg3)
@@ -16,13 +16,16 @@ export function unpartial(arg1, arg2, arg3?) {
 function unpartialTrio(superBase, base, partial) {
   if (superBase === undefined || superBase === null)
     return superBase
+
   if (base === undefined || base === null)
-    return base
+    return unpartial(superBase, partial)
+
   return defaults({}, partial, base, superBase)
 }
 
 function unpartialDuo(base, partial) {
-  if (base === undefined || base === null)
-    return base
+  if (partial === undefined || partial === null)
+    return partial
+
   return defaults({}, partial, base)
 }
