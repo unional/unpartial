84fa1ffdd89e6ca924d89d333eeb8293e3292084
Homa Wong <homawong@gmail.com>
2017-10-23 22:45:01 -0700

    Add unpartialRecursively (#9)

---
+++ src/unpartialRecursively.ts
@@ -0,0 +1,29 @@
+import merge = require('lodash.merge')
+
+export function unpartialRecursively<T extends object>(base: T, partial: object | undefined): T
+export function unpartialRecursively<T extends object, R extends object = {}>(superBase: R, base: T | undefined, partial: object | undefined): T & R
+export function unpartialRecursively(arg1, arg2, arg3?) {
+  if (arguments.length === 3) {
+    return unpartialTrio(arg1, arg2, arg3)
+  }
+  else {
+    return unpartialDuo(arg1, arg2)
+  }
+}
+
+function unpartialTrio(superBase, base, partial) {
+  if (superBase === undefined || superBase === null)
+    return superBase
+
+  if (base === undefined || base === null)
+    return unpartialRecursively(superBase, partial)
+
+  return merge({}, superBase, base, partial)
+}
+
+function unpartialDuo(base, partial) {
+  if (partial === undefined || partial === null)
+    return partial
+
+  return merge({}, base, partial)
+}
