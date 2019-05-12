49f13524a20af08154d492437312b36f534dbaa9
Homa Wong <homawong@gmail.com>
2019-05-11 23:34:08 -0700

    feat: add `requiredDeep()` (#24)
    * feat: add `requiredDeep()`
    
    fix: use `deepmerge` instead of `lodash.merge` internally.
    
    `lodash.merge` is big compare to `deepmerge`.
    
    Change the behavior when the first argument is undefined or null.
    This could be breaking but I consider it as not because the type prevent this from happening.
    And in normal usecase this should not happen.
    
    The new behavior is more consistent.
    
    * move size-limit check to its own job
    
    * remove running size-limit

--- src/required.ts
+++ src/required.ts
@@ -1,10 +1,23 @@
+import deepmerge from 'deepmerge'
+
 export function required<
-  T extends object,
-  R extends object = {},
-  S extends object = {}
+  T extends Record<any, any>,
+  R extends Record<any, any> = T,
+  S extends Record<any, any> = T & R
 >(source1: Partial<T>, source2: Partial<R> | undefined | null, source3?: Partial<S> | null): T & R & S {
-  let result = {}
-  const entries = [source1, source2, source3].filter(x => !!x)
-  entries.forEach(e => result = { ...result, ...e })
-  return result as any
+  return merge([source1, source2, source3], (p, e) => ({ ...p, ...e }))
+}
+
+export function requiredDeep<
+  T extends Record<any, any>,
+  R extends Record<any, any> = T,
+  S extends Record<any, any> = T & R
+>(source1: Partial<T>, source2: Partial<R> | undefined | null, source3?: Partial<S> | null): T & R & S {
+  return merge([source1, source2, source3], (p, e) => deepmerge(p, e))
+}
+
+function merge(entries: any[], reducer: (result: any, entry: any) => any) {
+  if (entries[0] === undefined || entries[0] === null) return entries[0]
+
+  return entries.filter(x => !!x).reduce(reducer, {} as any)
 }
