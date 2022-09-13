3d0a97807d9eca2f5ceebf6084cb38692a60bc82
Homa Wong <homawong@gmail.com>
2022-09-13 11:53:08 -0700

    chore: update dev deps

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,11 +1,5 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      isolatedModule: true,
-      useESM: true,
-    },
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
@@ -16,7 +10,16 @@ export default {
     '<rootDir>/ts',
   ],
   transform: {
+    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
+    '^.+\\.(ts|tsx|mts|cts)$': ['ts-jest', {
+      isolatedModules: true,
+      useESM: true,
+    }]
   },
+  // transformIgnorePatterns: [
+  //   // Need to MANUALLY identify each ESM package, one by one
+  //   'node_modules/(?!(chalk)/)'
+  // ],
   testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
   watchPlugins: [
     'jest-watch-suspend',
