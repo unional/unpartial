e11299de573bfea3f3086d79c627407618524d01
unional <homawong@gmail.com>
2022-12-06 21:13:21 -0800

    chore: use repobuddy/jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,41 +1,4 @@
+/** @type {import('jest').Config} */
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      isolatedModule: true,
-      useESM: true,
-    },
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  collectCoverageFrom: [
-    '<rootDir>/ts/**/*.[jt]s'
-  ],
-  roots: [
-    '<rootDir>/ts',
-  ],
-  transform: {
-    // '^.+\\.(js|jsx|mjs)$': 'babel-jest',
-    // '^.+\\.(ts|tsx|mts|cts)$': ['ts-jest', {
-    //   isolatedModules: true,
-    //   useESM: true,
-    // }]
-  },
-  // transformIgnorePatterns: [
-  //   // Need to MANUALLY identify each ESM package, one by one
-  //   'node_modules/(?!(chalk)/)'
-  // ],
-  testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
-  watchPlugins: [
-    'jest-watch-suspend',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-    [
-      'jest-watch-toggle-config', { 'setting': 'verbose' },
-    ],
-    [
-      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
-    ],
-  ],
+  preset: '@repobuddy/jest/presets/ts-esm'
 }
